7a392f3048e599e2826d465f7e35ec2a65019bc8
AryaNet <yoonesyoonest@gmail.com>
2025-08-16 12:08:21 +0330

    commit tamrin movie

---
+++ bonus/bonus2.1.py
@@ -0,0 +1,6 @@
+password = input("Enter password: ")
+
+while password != "password123":
+    password = input("Enter password: ")
+
+print("password was correct!")
---
+++ bonus/bonus2.2.py
@@ -0,0 +1,5 @@
+x = 1
+
+while 1 <= 6:
+    print(x)
+    x = x + 1
